ccba83fe30ef73a71bb6039cbaa64827e1aa7433
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2026-01-08 11:32:59 -0700

    Add README.md

--- obj/Debug/net7.0/spark.AssemblyInfo.cs
+++ obj/Debug/net7.0/spark.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945f3108edc7ca386699ebfbfab595598782da01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a221f3d2df769110c17c9c1efd1c01447a08c5c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
