1f800636c1b30a04c9d291063f9bef7f19320d15
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2026-01-14 02:05:37 -0700

    Refactor DTOs into new folder structure

--- backend/obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -10,11 +10,10 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7221a5c7df2ee2c2979d1cd88f572ae852116664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a539d3a8927e5e97bc66bfe5aaaacefc80a17b")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
